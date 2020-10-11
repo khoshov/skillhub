baea7b995244213875196973d0ba7bb992c7dcf1
Stanislav Khoshov <khoshov@gmail.com>
2020-10-11 23:16:54 +0300

    update models

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
@@ -13,45 +13,45 @@ class Course(models.Model):
     ]
 
     name = models.CharField(
-        verbose_name=_('Название'),
+        _('Название'),
         max_length=255,
     )
     category = models.ManyToManyField(
-        to='courses.Category',
+        'courses.Category',
         through='courses.CourseCategory',
         verbose_name=_('Категория'),
     )
     school = models.ForeignKey(
-        to='schools.School',
-        on_delete=models.CASCADE,
+        'schools.School',
+        models.CASCADE,
         verbose_name=_('Школа'),
     )
     type = models.PositiveIntegerField(
-        verbose_name=_('Тип'),
+        _('Тип'),
         choices=TYPES,
         default=ONLINE,
     )
     price = models.DecimalField(
-        verbose_name=_('Цена ₽'),
+        _('Цена ₽'),
         max_digits=9,
         decimal_places=2,
         null=True, blank=True,
     )
     duration = models.PositiveIntegerField(
-        verbose_name=_('Длительность, мес.'),
+        _('Длительность, мес.'),
         null=True, blank=True,
     )
     start_date = models.DateField(
-        verbose_name=_('Дата старта'),
+        _('Дата старта'),
         null=True, blank=True,
     )
     certificate = models.BooleanField(
-        verbose_name=_('Выдается сертификат гос. образца'),
+        _('Выдается сертификат гос. образца'),
         default=False,
     )
 
     class Meta:
-        db_table = 'courses'
+        db_table = 'course'
         verbose_name = _('Курс')
         verbose_name_plural = _('Курсы')
 
@@ -61,19 +61,19 @@ class Course(models.Model):
 
 class Category(MPTTModel):
     parent = TreeForeignKey(
-        to='self',
-        on_delete=models.CASCADE,
+        'self',
+        models.CASCADE,
         null=True, blank=True,
         related_name='children',
         verbose_name=_('Родительская категория')
     )
     name = models.CharField(
-        verbose_name=_('Имя'),
+        _('Имя'),
         max_length=255,
     )
 
     class Meta:
-        db_table = 'categories'
+        db_table = 'category'
         verbose_name = _('Категория')
         verbose_name_plural = _('Категории')
 
@@ -83,12 +83,12 @@ class Category(MPTTModel):
 
 class CourseCategory(models.Model):
     course = models.ForeignKey(
-        to='courses.Course',
-        on_delete=models.CASCADE,
+        'courses.Course',
+        models.CASCADE,
     )
     category = models.ForeignKey(
-        to='courses.Category',
-        on_delete=models.CASCADE,
+        'courses.Category',
+        models.CASCADE,
     )
 
     class Meta:
--- apps/schools/models.py
+++ apps/schools/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class School(models.Model):
     name = models.CharField(
-        verbose_name=_('Название'),
+        _('Название'),
         max_length=255,
     )
     description = models.TextField(
-        verbose_name=_('Описание'),
+        _('Описание'),
         null=True, blank=True,
     )
 
     class Meta:
-        db_table = 'schools'
+        db_table = 'school'
         verbose_name = _('Школа')
         verbose_name_plural = _('Школы')
 
