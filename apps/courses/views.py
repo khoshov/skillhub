1cfc9ba7ceceacb9948b085baf35a0766ea76227
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2021-07-13 02:28:23 +0400

    update course upload view permissions

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets
 from rest_framework.parsers import JSONParser
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_api_key.permissions import HasAPIKey
@@ -15,8 +14,7 @@ class CourseViewSet(viewsets.ModelViewSet):
 
 
 class UploadCourseAPIView(APIView):
-    # permission_classes = [HasAPIKey]
-    permission_classes = [AllowAny]
+    permission_classes = [HasAPIKey]
     parser_classes = [JSONParser]
 
     def post(self, request, format=None):
