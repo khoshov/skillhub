adb7e3c91968f6efa12328b5c992837daac5b115
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2021-10-31 00:27:11 +0400

    clean up

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -15,6 +10,4 @@ urlpatterns = [
     path('schools/', include('schools.urls', namespace='schools')),
 
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-
-    path('sentry-debug/', trigger_error),
 ]
