548f400f407f9cb6e1432e54601dffa9f7a1c937
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2021-10-30 23:25:54 +0400

    add sentry

--- config/settings.py
+++ config/settings.py
@@ -162,3 +162,21 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+if not DEBUG:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn="https://ba55c2cb70a742a591c8d389c4cd3748@o1056192.ingest.sentry.io/6042376",
+        integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
