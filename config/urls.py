dc4cfec5df8df49dbe627d6f80d30d1f89a2a19c
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2021-10-31 12:43:32 +0400

    split settings file

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,3 @@
-import debug_toolbar
-
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
@@ -16,5 +14,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
 
