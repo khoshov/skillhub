79144e287bce175c346d18c9c5e221cb72ca82f3
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-01-06 22:49:12 +0400

    updates

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -6,6 +6,6 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='list'),
-    path('<slug:slug>/', CourseListView.as_view(), name='category'),
     path('upload/', UploadCourseAPIView.as_view(), name='upload'),
+    path('<slug:slug>/', CourseListView.as_view(), name='category'),
 ]
