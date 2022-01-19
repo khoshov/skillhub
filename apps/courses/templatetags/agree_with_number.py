4516d10865448afe5707fe775320b652a616b738
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-01-19 23:36:27 +0400

    add agree with number filter

--- apps/courses/templatetags/agree_with_number.py
+++ apps/courses/templatetags/agree_with_number.py
@@ -7,6 +7,4 @@ register = template.Library()
 
 @register.filter
 def agree_with_number(value, num):
-    print(value)
-    print(num)
     return morph.parse(value)[0].make_agree_with_number(num).word
