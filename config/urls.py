fd70901e36f03b32b5e917d1201c78e7c77e93de
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-03-17 23:58:39 +0400

    update 404

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,17 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from core.views import page_not_found, server_error
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('courses/', include('courses.urls', namespace='courses')),
     path('reviews/', include('reviews.urls', namespace='reviews')),
     path('schools/', include('schools.urls', namespace='schools')),
+
+    path('404/', TemplateView.as_view(template_name='404.html')),
+    path('500/', TemplateView.as_view(template_name='500.html')),
 
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
