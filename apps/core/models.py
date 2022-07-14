b9a92c60060008d30de9e513c651fda329b19c2a
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-07-14 13:00:04 +0400

    update courses models

--- apps/core/models.py
+++ apps/core/models.py
@@ -36,3 +36,37 @@ class MainPageConfig(ModelMeta, SingletonModel):
 
     class Meta:
         verbose_name = _('Главная страница')
+
+
+class AllCoursesPageConfig(ModelMeta, SingletonModel):
+    title = models.CharField(
+        _('Заголовок'),
+        max_length=255,
+        blank=True, null=True,
+
+    )
+    description = models.TextField(
+        _('Описание'),
+        blank=True, null=True,
+    )
+
+    meta_title = models.CharField(
+        _('Meta title'),
+        max_length=255,
+        blank=True, null=True,
+    )
+    meta_description = models.TextField(
+        _('Meta description'),
+        blank=True, null=True,
+    )
+
+    _metadata = {
+        'title': 'meta_title',
+        'description': 'meta_description',
+    }
+
+    def __str__(self):
+        return 'Страница всех курсов'
+
+    class Meta:
+        verbose_name = _('Страница всех курсов')
