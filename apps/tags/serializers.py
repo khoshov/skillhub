f5850040627d04dc3fcb6c28300ddfa21b7d9aa6
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-12-06 23:24:06 +0300

    update tags serializers

--- apps/tags/serializers.py
+++ apps/tags/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from tags.models import Tag, TagMatch
+from tags.models import Tag, TagMatch, TagOption
+
+
+class TagOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TagOption
+        fields = '__all__'
 
 
 class TagSerializer(serializers.ModelSerializer):
+    options = TagOptionSerializer(many=True)
+
     class Meta:
         model = Tag
         fields = '__all__'
