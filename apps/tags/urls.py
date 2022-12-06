f04a0a2ada80c5e706e15648b28750f6d46926cf
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-12-06 23:19:08 +0300

    update tags urls

--- apps/tags/urls.py
+++ apps/tags/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from .views import TagMatchListCreateAPIView, TagListAPIView
 
-app_name = 'reviews'
+app_name = 'tags'
 
 urlpatterns = [
-    path('', TagMatchListCreateAPIView.as_view(), name='list'),
-    path('match/', TagListAPIView.as_view(), name='match'),
+    path('', TagListAPIView.as_view(), name='list'),
+    path('match/', TagMatchListCreateAPIView.as_view(), name='match'),
 ]
